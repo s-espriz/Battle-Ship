c79a9a5ed86026775e4d7f88717eb52940e5fd96
Hossei Nikdel <nikdel@mail.com>
2019-07-05 01:00:59 +0430

    Final Repo

--- progephase121211.c
+++ progephase121211.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 int length_1 ; 
 int width_1 ;
-int tedad_kashti;	
+int tedad_kashti_1 = 0;	
+int tedad_kashti_2 = 0;
+int money_1 ;   
+int money_2 ; 
+#define Price_a 10
+#define Price_b 20
+
 #define MAX 100
 void clrscr()
 {
@@ -17,23 +24,28 @@ void init_map(){
 	scanf("%d", &width);
 	printf("\nEnter length:");
 	scanf("%d" , &length);
-	printf("\nHow Many Ships:");
-	scanf("%d" , &ship_count);
+	// printf("\nHow Many Ships:");
+	// scanf("%d" , &ship_count);
 	clrscr();
-	tedad_kashti = ship_count;
+	// tedad_kashti = ship_count;
 	length_1 = length;
 	width_1 = width;
+	ship_count = (int)sqrt(length*width);
+	money_1 = money_2 = 15 * ship_count;
 }
 struct information{
      char tol[100], arz[100];
-	char kind[100], jahat[100];
+	 char kind[100], jahat[100];
    };
+
    struct information player[2];
     void locate_info(void);
     void gameplay(int array1[][width_1], int array2[][width_1]);
     void show(int array[][width_1]);
     void anjame_bazi(char *str3, int array[][width_1]);
-    int check(int array[][width_1]);
+    int check1(int array[][width_1]);
+    int check2(int array[][width_1]);
+
 
 int main(){
 	init_map();
@@ -42,9 +54,12 @@ int main(){
 	int array1[length_1][width_1];
   	int array2[length_1][width_1];
 	locate_info();
+	// printf("%d\n",tedad_kashti_1 );
+	// printf("%d\n",tedad_kashti_2 );
+
 	gameplay(array1, array2);
 	
-	while(((check(array1)) && (check(array2)))){
+	while(((check1(array1)) && (check2(array2)))){
 		char str_1[10];
 	if((tester % 2) == 0){
 		printf("Player1 : \nEnter a point\n");
@@ -66,44 +81,152 @@ int main(){
 	}
 	++tester;  
 	}	
-	if((check(array1)) == 0)
+	if((check1(array1)) == 0)
 	{
 		clrscr();
 		printf("player 2 win!\n");
 	}
-	if((check(array2)) == 0)
+	if((check2(array2)) == 0)
 	{	
 		clrscr();
 		printf("player 1 win!\n");
 	}
 }
 void locate_info(void){
-	int i, j;
-   for(j = 0; j < 2; ++j){
-   	printf("Enter your ships location:");
-   	for(i = 0; i < tedad_kashti; ++i){
-   		char str[50];
-		fgets(str , MAX ,stdin);
-		if(str[0] == '\n'){
-			str[0] = '\0';
-			i--;
-			continue;
+	// int ship_counter = 0 ;
+    char str[50];
+	while(1)
+	{
+		if(money_1 == 0)
+		{
+			break;
 		}
-	if((str[5] < 48) || (str[5] > (length_1 + 48)) || (str[7] < 48) || (str[7] > (width_1 + 48))){
-		printf("This location is not defined!\n");
-		--i;
-		continue;
-	}	
-	player[j].kind[i] = str[0];
-	player[j].tol[i] = str[5];
-	player[j].arz[i] = str[7];
-	player[j].jahat[i] = str[15];
-	printf("\n");
-	clrscr();
+		printf("Player 1 :\nEnter Your Ships \nYour Money:%d\n>>", money_1);
+		fgets(str , MAX ,stdin);
+			if(str[0] == '\n'){
+				str[0] = '\0';
+				clrscr();
+				continue;
+			}
+
+		if(str[0] == 'A'){
+			money_1 = money_1 - Price_a ;
+			if(money_1 >= 0)
+			{
+				player[1].kind[tedad_kashti_1] = str[0];
+				player[1].tol[tedad_kashti_1] = str[5];
+				player[1].arz[tedad_kashti_1] = str[7];
+				player[1].jahat[tedad_kashti_1] = str[15];
+								printf("%d\n",tedad_kashti_1 );
+
+				tedad_kashti_1++;
+				clrscr();
+			}
+			else{
+				printf("You Have Not Enought Money\n");
+				break;
+			}
+		}
+		if (str[0] == 'B')
+		{
+
+			money_1 = money_1 - Price_b ;
+			if(money_1 >= 0)
+			{
+				player[0].kind[tedad_kashti_1] = str[0];
+				player[0].tol[tedad_kashti_1] = str[5];
+				player[0].arz[tedad_kashti_1] = str[7];
+				player[0].jahat[tedad_kashti_1] = str[15];
+				printf("%d\n",tedad_kashti_1 );
+				tedad_kashti_1++;
+				clrscr();
+			}
+			else{
+				printf("You Have Not Enought Money\n");
+	
+			}
+
+
+
+	}
 }
-  printf("\n");
-  }
+	while(1)
+	{
+		if(money_2 == 0)
+		{
+			break;
+		}
+		printf("Player 2 :\nEnter Your Ships \nYour Money:%d\n>>", money_2);
+   		
+		fgets(str , MAX ,stdin);
+			if(str[0] == '\n'){
+				str[0] = '\0';
+				clrscr();
+				continue;
+			}
+		if(str[0] == 'A'){
+			money_2 = money_2 - Price_a ;
+			if(money_2 >= 0)
+			{
+				player[1].kind[tedad_kashti_2] = str[0];
+				player[1].tol[tedad_kashti_2] = str[5];
+				player[1].arz[tedad_kashti_2] = str[7];
+				player[1].jahat[tedad_kashti_2] = str[15];
+
+				printf("%d\n",tedad_kashti_2 );
+				tedad_kashti_2++;
+				clrscr();
+			}
+			else{
+				printf("You Have Not Enought Money\n");
+				break;
+			}
+		}
+		if (str[0] == 'B')
+		{
+
+			money_2 = money_2 - Price_b ;
+			if(money_2 >= 0)
+			{
+				player[1].kind[tedad_kashti_2] = str[0];
+				player[1].tol[tedad_kashti_2] = str[5];
+				player[1].arz[tedad_kashti_2] = str[7];
+				player[1].jahat[tedad_kashti_2] = str[15];
+				tedad_kashti_2++;
+
+				clrscr();
+			}
+			else{
+				printf("You Have Not Enght Money\n");
+				
+			}
+		}
+
+ //   for(j = 0; j < 2; ++j){
+ //   	printf("Enter your ships location:");
+ //   	for(i = 0; i < tedad_kashti; ++i){
+ //   		char str[50];
+	// 	fgets(str , MAX ,stdin);
+	// 	if(str[0] == '\n'){
+	// 		str[0] = '\0';
+	// 		i--;
+	// 		continue;
+	// 	}
+	// if((str[5] < 48) || (str[5] > (length_1 + 48)) || (str[7] < 48) || (str[7] > (width_1 + 48))){
+	// 	printf("This location is not defined!\n");
+	// 	--i;
+	// 	continue;
+	// }	
+	// player[j].kind[i] = str[0];
+	// player[j].tol[i] = str[5];
+	// player[j].arz[i] = str[7];
+	// player[j].jahat[i] = str[15];
+	// printf("\n");
+	// clrscr();
 }
+  // printf("\n");
+}
+
  void gameplay(int array1[][width_1], int array2[][width_1]){
  	int m, n, l, pot;
  	int tol1, arz1, tol2, arz2;
@@ -113,18 +236,27 @@ void locate_info(void){
    	 	     array2[m][n] = 0;
    	 	}
    	 }
-  	    for(l = 0; l < tedad_kashti; ++l){
+  	    for(l = 0; l < tedad_kashti_1; ++l){
   	     tol1 = (player[0].tol[l] - 48);
   	     arz1 = (player[0].arz[l] - 48);
-  	     array1[tol1][arz1] = 1;
+  	     if(player[0].kind[l] == 'A')
+  	     	array1[tol1][arz1] = 1;
+  	     if(player[0].kind[l] == 'B')
+  	     	array1[tol1][arz1] = 2;
+  	     
 		 }
-		  for(pot = 0; pot < tedad_kashti; ++pot){
+		  for(pot = 0; pot < tedad_kashti_2; ++pot){
   	     tol2 = (player[1].tol[pot] - 48);
   	     arz2 = (player[1].arz[pot] - 48);
-  	   	    array2[tol2][arz2] = 1;
+			  	   	    // array2[tol2][arz2] = 1;
+  	     if(player[1].kind[pot] == 'A')
+  	     	array2[tol2][arz2] = 1;
+  	     if(player[1].kind[pot] == 'B')
+  	     	array2[tol2][arz2] = 2;
+
 		 }
   }
-  //show the plan
+  // show the plan/
   void show(int array[][width_1]){
   	int array_new[length_1][width_1];
   	  int r, s;
@@ -136,6 +268,10 @@ void locate_info(void){
 		if((array[r][s]) == 1){
    	 			array_new[r][s] = 63;
 				}
+		if((array[r][s]) == 2){
+   	 			array_new[r][s] = 63;
+				}
+
 		if((array[r][s]) == 7){
    	 			array_new[r][s] = 42;
 				}
@@ -152,7 +288,7 @@ void locate_info(void){
   }		
 }
   //check end of game
-   int check(int array[][width_1]){
+   int check1(int array[][width_1]){
    	int a, b, counter = 0;
    	 for(a = 0; a < length_1; ++a){
    	 	for(b = 0; b < width_1; ++b){
@@ -161,11 +297,26 @@ void locate_info(void){
 				}
 			}
 		}
-   	  if(counter == tedad_kashti){
+   	  if(counter == tedad_kashti_1){
    	  	return 0;
 		 }
 	 return 1;
    }
+   int check2(int array[][width_1]){
+   	int a, b, counter = 0;
+   	 for(a = 0; a < length_1; ++a){
+   	 	for(b = 0; b < width_1; ++b){
+   	 		if((array[a][b]) == 7){
+   	 			++counter;
+				}
+			}
+		}
+   	  if(counter == tedad_kashti_2){
+   	  	return 0;
+		 }
+	 return 1;
+   }
+
   void anjame_bazi(char *str3, int array[][width_1]){
   	int q;
 	  q = (str3[1] - 48);
@@ -177,6 +328,10 @@ void locate_info(void){
   	  if(array[q][e] == 1){
   		array[q][e] = 7;
 	  }
+  	  if(array[q][e] == 2){
+  		array[q][e] = 1;
+	  }
+
   }
 
 
